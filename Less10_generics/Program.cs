fbebdbb227874a147febaa76b5d7da811398bf16
Natalya Babenko <ifelseiforelif@gmail.com>
2026-01-09 19:06:18 +0200

    added generics

---
+++ Less10_generics/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+namespace Less10_generics;
+
+
+class Box<T>
+{
+    public T value;
+    public Box(T content) 
+    {
+        this.value = content;
+    }
+    public T GetValue()
+    {
+        return value;
+    }
+    public void Print()
+    {
+        Console.WriteLine(value);
+    }
+}
+
+internal class Program
+{
+    /*
+     * Створити generic клас Pair<T1, T2> 
+     * для зберігання двох значень різного типу.
+Додати метод Display(), який виводить обидва значення.
+    Pair<string, int> p1 = new Pair<string, int>();
+    p1.Add("bread", 30);
+    p1.Display();
+     */
+    static void Main(string[] args)
+    {
+        List<string> names = new List<string>();
+        names.Add("Bob");
+        names.Add("Alice");
+        names.Add("John");
+        Stack<string> stack = new Stack<string>();
+        LinkedList<string> list = new LinkedList<string>();
+
+        Box<int> box1 = new Box<int>(10);
+        Console.WriteLine(box1.GetValue());
+        Box<string> box2 = new Box<string>("Hi");
+        Console.WriteLine(box2.GetValue());
+
+        //Hashtable ht = new Hashtable();
+        //ht["Alice"] = 20;
+
+        //ArrayList list = new ArrayList();
+        //list.Add(10);
+        //list.Add(true);
+        //list.Add("Hi");
+        //Console.WriteLine(list[2]);
+        //Console.WriteLine((int)list[0]*2);
+
+        //Box box1 = new Box(1); //int
+        //Box box2 = new Box(4.5); //double
+        //Box box3 = new Box("Hello"); //string
+        //Box box4 = new Box(box1); //string
+
+        //int x = (int)box1.value;
+        //Console.WriteLine(x);
+
+    }
+}
--- Less9_records_extensions/Program.cs
+++ Less9_records_extensions/Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace Less9_records_extensions;
 using Less9_records_extensions.MyExt;
+using System.Runtime.InteropServices;
+
 internal static class MyMath
 {
     public static void Sum(int a, int b)
@@ -9,25 +11,61 @@ internal static class MyMath
 }
 
 //Extensions - методи розширення
+//records
+
+//C#9, C#10
+public record Animal();
+public record Cat(string Name, string Color, short Age)
+{
+    public string Name { get; set; }
+    public readonly string Color { get; init; }
+}
+public record struct Dog(string Name, string Color, short Age)
+{
+
+}
+
+//get; init
 
 internal class Program
 {
-    static void Change(int a)
-    {
-        a++;
-    }
-    static void Change(ref int[] arr)
-    {
-        arr = new int[arr.Length];
-        arr[0] = 100;
-    }
+    
     static void Main(string[] args)
     {
-        int n = 10;
-        n.MyUp(23);
-        Console.WriteLine(n);
-        Student std = new Student("Bob", "Smith", "PV521");
-        std.WelcomeStudent();
+
+        Dog dog1 = new Dog("Jack", "Black", 3);
+        dog1.Age ++;
+        Console.WriteLine(dog1);
+        Cat cat = new Cat("Grey", "white", 2);
+        cat = cat with
+        {
+            Age = 3,
+            Color = "Brown"
+        };
+
+        Cat cat2 = new Cat("Grey", "white", 2);
+        if(cat==cat2)
+        {
+            Console.WriteLine("Equals");
+        }
+        if(ReferenceEquals(cat, cat2))
+        {
+            Console.WriteLine("Equals");
+        }
+        //cat.Name = "White"; //IMMUTABLE
+        Console.WriteLine(cat);
+        Student std1 = new Student("Bob", "Smith", "PV521");
+        Student std2 = new Student("Bob", "Smith", "PV521");
+        if(std1==std2)
+        {
+            Console.WriteLine("Equals");
+        }
+
+        //int n = 10;
+        //n.MyUp(23);
+        //Console.WriteLine(n);
+        //Student std = new Student("Bob", "Smith", "PV521");
+        //std.WelcomeStudent();
 
         //MyMath.Sum(3,4);
         //int[] arr = { 4, 5, 6 };
@@ -36,5 +74,14 @@ internal class Program
         //{
         //    Console.WriteLine(el);
         //}
+    }
+    static void Change(int a)
+    {
+        a++;
+    }
+    static void Change(ref int[] arr)
+    {
+        arr = new int[arr.Length];
+        arr[0] = 100;
     }
 }
